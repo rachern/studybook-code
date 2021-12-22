37b16cc74ec2129169eb2c01db3254aa2ca7ce19
rachern <455485327@qq.com>
2021-12-23 01:41:54 +0800

    leetcode-621 任务调度器

---
+++ algorithm/task-scheduler.js
@@ -0,0 +1,21 @@
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+ var leastInterval = function(tasks, n) {
+    const obj = {}
+    for (let i = 0; i < tasks.length; i++) {
+        if (!obj[tasks[i]]) {
+            obj[tasks[i]] = 1
+        } else {
+            obj[tasks[i]]++
+        }
+    }
+    const max = Math.max(...Object.values(obj))
+    let maxCount = 0
+    Object.values(obj).forEach(i => {
+        if (i === max) maxCount++
+    })
+    return Math.max((max - 1) * (n + 1) + maxCount, tasks.length)
+};
