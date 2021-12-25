0a183f57dc0a053572835c91fa3b6cdfd99b312b
rachern <455485327@qq.com>
2021-12-25 22:37:31 +0800

    leetcode-20 有效的括号

---
+++ algorithm/valid-parentheses.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s) {
+    const stack = []
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            stack.push(s[i])
+            continue
+        } 
+        const val = stack.pop()
+        switch (s[i]) {
+            case ')':
+                if (val !== '(') {
+                    return false
+                }
+                break
+            case '}':
+                if (val !== '{') {
+                    return false
+                }
+                break
+            case ']':
+                if (val !== '[') {
+                    return false
+                }
+                break
+        }
+    }
+    return !stack.length
+};
