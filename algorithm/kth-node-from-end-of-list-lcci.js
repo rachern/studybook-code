121171f44f6a4227c26b6aa0548becb4cbcfb95b
rachern <455485327@qq.com>
2022-01-02 00:26:43 +0800

    面试题 02.02. 返回倒数第 k 个数（解法二）

--- algorithm/kth-node-from-end-of-list-lcci.js
+++ algorithm/kth-node-from-end-of-list-lcci.js
@@ -6,6 +6,7 @@
  * }
  */
 /**
+ * @desc 双指针
  * @param {ListNode} head
  * @param {number} k
  * @return {number}
@@ -23,5 +24,31 @@
         q = q.next
     }
     // 返回 p 的值，即为倒数第 k 个节点
+    return p.val
+};
+
+/**
+ * @desc 两次循环
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {number}
+ */
+var kthToLast = function(head, k) {
+    // 定义 n 记录链表长度，p 指针指向头节点
+    let n = 0, p = head
+    // p 指针遍历整个链表，记录链表长度
+    while (p) {
+        p = p.next
+        n++
+    }
+    // p 指针从头节点重新开始
+    p = head
+    // 计算 n - k 的值，即 p 指针需要走几步到达倒数第 k 个节点
+    let t = n - k
+    // p 指针往前走 t 步
+    while (t--) {
+        p = p.next
+    }
+    // 返回 p 指针指向节点的值
     return p.val
 };
