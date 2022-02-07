9c3c6d6f5e1343cea08ccaf43a0b7e4ccee9b81d
rachern <455485327@qq.com>
2022-02-07 23:00:35 +0800

    leetcode-100 相同的树

--- algorithm/same-tree.js
+++ algorithm/same-tree.js
@@ -7,6 +7,7 @@
  * }
  */
 /**
+ * @desc 深度优先
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {boolean}
@@ -15,4 +16,25 @@
     if (!p && !q) return true
     if (p && !q || !p && q || p.val !== q.val) return false
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
+
+/**
+ * @desc 广度优先
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+ var isSameTree = function(p, q) {
+    const queue = [p, q]
+    while (queue.length) {
+        const node1 = queue.shift()
+        const node2 = queue.shift()
+        if (!node1 && !node2) continue
+        if (node1 && !node2 || !node1 && node2 || node1.val !== node2.val) return false
+        queue.push(node1.left)
+        queue.push(node2.left)
+        queue.push(node1.right)
+        queue.push(node2.right)
+    }
+    return true
 };
