e8e41a5669269e8ef930301dacabbf30c9a2998f
rachern <455485327@qq.com>
2022-03-02 00:56:09 +0800

    leetcode-470 用 Rand7() 实现 Rand10()

--- algorithm/implement-rand10-using-rand7.js
+++ algorithm/implement-rand10-using-rand7.js
@@ -15,3 +15,17 @@
         if (num <= 20) return 1 + num % 10
     }
 };
+
+/**
+ * The rand7() API is already defined for you.
+ * var rand7 = function() {}
+ * @return {number} a random integer in the range 1 to 7
+ */
+ var rand10 = function() {
+    let p = rand7(), q = rand7()
+    // p 实现 rand5
+    while (p > 5) p = rand7()
+    while (q === 7) q = rand7()
+    // q 大于 3 的概率是 1/2，可以看成 rand2
+    return q > 3 ? p : p + 5
+};
