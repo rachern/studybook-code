0ee9a9aff091d1ac07aca588941cca53ecfb820c
rachern <455485327@qq.com>
2022-06-09 23:04:54 +0800

    面试题 04.08. 首个共同祖先

--- algorithm/first-common-ancestor-lcci.js
+++ algorithm/first-common-ancestor-lcci.js
@@ -6,6 +6,7 @@
  * }
  */
 /**
+ * @desc 解法一
  * @param {TreeNode} root
  * @param {TreeNode} p
  * @param {TreeNode} q
@@ -26,3 +27,26 @@
     dfs(root, p, q, ans)
     return ans
 };
+
+
+/**
+ * @desc 解法二
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+ var lowestCommonAncestor = function(root, p, q) {
+    const ans = dfs(root, p, q)
+    return ans
+};
+
+var dfs = function (root, p, q) {
+    if (!root) return null
+    if (root === p || root === q) return root
+    const fl = dfs(root.left, p, q)
+    const fr = dfs(root.right, p, q)
+    if (fl && fr) return root
+    if (fl) return fl
+    return fr
+}
